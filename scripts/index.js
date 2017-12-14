e01caff6156cc7b119b16e28ddc472542bd28b68
junkkerrigan <726721gfd>
2017-12-14 23:00:20 +0200

    completed responsive menu

--- scripts/index.js
+++ scripts/index.js
@@ -1,13 +1,44 @@
+$(document).ready( function () {
+
+});
+
 $(window).on('scroll', function () {
-   if ($(this).scrollTop() >= 50) {
+    var self = $(this);
+    var listMenu = $('.fixed-item');
+    var curIndex = -1;
+
+    $('section').each(function (index) {
+        if (self.scrollTop() - ($(this).outerHeight()/2) >
+            $(this).offset().top) {
+            curIndex = index;
+        }
+    });
+
+    if (curIndex === -1) {
+        listMenu.removeClass('active');
+    }
+    else {
+        var curItemMenu = listMenu.eq(curIndex);
+
+        if (!curItemMenu.hasClass('active')) {
+            curItemMenu.addClass('active');
+            curItemMenu.siblings().removeClass('active');
+        }
+    }
+
+    if ($(this).scrollTop() >= 50) {
         $('.fixed').addClass('fix');
     }
 
     else {
-       $('.fixed').removeClass('fix');
-   }
+        $('.fixed').removeClass('fix');
+    }
 });
 
 $('.fixed-toggle').on('click', function () {
     $('.fixed-list').toggleClass('active');
+    $('.fixed-toggle').toggleClass('active');
 });
+
+
+
